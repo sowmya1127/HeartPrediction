4b63a7f600e3c752bbca7084ad35059584b8686e
sowmya1127 <eppalpalli.sowmya6@gmail.com>
2022-10-03 08:45:06 +0530

    HeartAttack-Prediction

--- model.py
+++ model.py
@@ -18,12 +18,17 @@ y=df['TenYearCHD']
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=0)
 
-from sklearn.linear_model import LogisticRegression
-lg=LogisticRegression()
-lg.fit(X_train,y_train)
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=20, random_state=0)
+regressor.fit(X_train, y_train)
 
 
 import pickle
 
-pickle.dump(lg, open('model.pkl','wb'))
+pickle.dump(regressor, open('model.pkl','wb'))
 model=pickle.load(open('model.pkl','rb'))
